8878417183950b3f1841fa14ef01040c9cb4f7fc
Shafa Asyari Ramadhan <shafa.asyari.02@gmail.com>
2023-11-04 02:16:22 +0800

    error handling getRinjaniCultureAPI

--- src/libs/api.js
+++ src/libs/api.js
@@ -1,8 +1,34 @@
-const getRinjaniCultureAPI = async (resource, query) => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/${resource}?${query}`)
-  const result = await response.json()
+const getRinjaniCultureAPI = async (resource, options, query) => {
+  try {
+    let apiUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}/${resource}`
 
-  return result
+    if (query) {
+      const queryString = new URLSearchParams(query).toString();
+      apiUrl = `${apiUrl}?${queryString}`;
+    }
+
+    const response = await fetch(apiUrl, {
+      ...options,
+    });
+
+    if (!response.ok) {
+      process.env.NEXT_PUBLIC_API_BASE_URL
+    }
+
+    const data = await response.json()
+    return data
+  } catch (error) {
+    // Handle errors here, such as network errors or API errors
+    console.error('Error fetching data:', error);
+    // Rethrow the error for the caller to handle
+    throw error;
+  }
 }
 
-export {getRinjaniCultureAPI}
+// const getRinjaniCultureAPI = async (resource, query) => {
+//   const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/${resource}?${query}`)
+//   // const result = await response.json()
+//   return response
+// }
+
+export { getRinjaniCultureAPI }
