331a7b9424c57b37c97221a1e54e13cb042fdf3a
Shafa Asyari Ramadhan <shafa.asyari.02@gmail.com>
2023-11-08 19:46:17 +0800

    add method for search package with params: id

---
+++ src/components/navbar/SearchInput.jsx
@@ -0,0 +1,35 @@
+import { MagnifyingGlass } from "@phosphor-icons/react"
+import { useRouter } from "next/navigation"
+import { useRef } from "react"
+
+const SearchInput = () => {
+  const searchRef = useRef()
+  const router = useRouter()
+
+  const hanldeSeacrh = (event) =>{
+    event.preventDefault()
+    const keyword = searchRef.current.value.trim()
+    if(keyword){
+      router.push(`/packages/${keyword}`)
+    }else{
+      alert('isi dulu dong')
+    }
+  }
+
+  return (
+    <div className="flex items-center h-10 rounded-lg overflow-hidden">
+      <input
+        ref={searchRef}
+        placeholder='Search Package'
+        type="search"
+        className="px-4 h-10 focus:outline-none"
+        onKeyPress={(event) => event.key === 'Enter' && hanldeSeacrh(event) }
+      />
+      <button className='cursor-pointer bg-green-600 h-full px-2' onClick={hanldeSeacrh}>
+        <MagnifyingGlass size={24} weight='bold' color='white' />
+      </button>
+    </div>
+  )
+}
+
+export default SearchInput
