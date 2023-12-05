84abc00ec91aabb0204bc3d4fddbd4209df9c6bb
Magistra <magistraagis@gmail.com>
2023-12-05 13:15:08 +0800

    show product

--- src/app/(user)/homestay/page.jsx
+++ src/app/(user)/homestay/page.jsx
@@ -1,4 +1,6 @@
+
 import HeaderPackage from "@/components/HeaderPackage";
+import CardPackageList from "@/components/package/CardPackageList";
 import FilterPackage from "@/components/package/filter/Filter";
 
 const Page = () => {
@@ -9,8 +11,8 @@ const Page = () => {
         <div className="border">
           <FilterPackage />
         </div>
-        <div className="border border-slate-900 col-span-4">
-          as
+        <div className=" col-span-4">
+          <CardPackageList/>
         </div>
       </div>
     </div>
