0cce76f1edc63d651aaa2e78028bf079355105c9
Magistra <magistraagis@gmail.com>
2023-12-05 13:15:18 +0800

    add responsive

--- src/components/HeaderPackage.jsx
+++ src/components/HeaderPackage.jsx
@@ -1,8 +1,8 @@
 const HeaderPackage = ({title, subtitle}) => {
   return (
     <div className="mb-4 space-y-1">
-      <h1 className="text-3xl">{title}</h1>
-      <p className="text-lg">{subtitle}</p>
+      <h1 className="text-3xl font-bold text-green-900 max-sm:text-xl">{title}</h1>
+      <p className="text-lg text-slate-800 max-sm:text-sm">{subtitle}</p>
     </div>
   )
 }
