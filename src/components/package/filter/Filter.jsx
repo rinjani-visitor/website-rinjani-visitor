a34983417967ad63f760b253c8f71402a8473426
Magistra <magistraagis@gmail.com>
2023-12-05 11:31:29 +0800

    add responsive for mobile view

--- src/components/package/filter/Filter.jsx
+++ src/components/package/filter/Filter.jsx
@@ -9,7 +9,7 @@ import StatusFilter from "./StatusFilter"
 const Filter = () => {
   const pathName = usePathname()
   return (
-    <div className="space-y-4">
+    <div className=" max-lg:flex max-lg:justify-between border border-red-600 max-lg:w-8/12 max-sm:w-full">
       {
         pathName.includes('package') ?
           <CategoryFilter />
