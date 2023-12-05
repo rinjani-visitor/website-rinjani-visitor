c30d0f5dcd4d48b3d82ade4ef7f3983f7137f745
Magistra <magistraagis@gmail.com>
2023-12-05 15:50:55 +0800

    add responsive

--- src/app/(user)/event/page.jsx
+++ src/app/(user)/event/page.jsx
@@ -19,7 +19,6 @@ export default function page() {
                 </div>
             </div>
         </div>
-        
     </div>
   )
 }
--- src/app/(user)/homestay/page.jsx
+++ src/app/(user)/homestay/page.jsx
@@ -6,15 +6,19 @@ import FilterPackage from "@/components/package/filter/Filter";
 const Page = () => {
   return (
     <div className="container">
-      <HeaderPackage title={`Home Stay`} subtitle={`Explore rinjani mountain and feel to live with local people`} />
-      <div className="grid grid-cols-5">
-        <div className="border">
-          <FilterPackage />
+        <div className='w-7/12 max-sm:w-full'>
+            <HeaderPackage title={`Homestay`} subtitle={`Rinjani Visitor is a service to accommodate tourists in enjoying the beauty of Mount Rinjani and the culture of Lombok`} />
         </div>
-        <div className=" col-span-4">
-          <CardPackageList/>
+        <div className='xl:flex xl:space-x-6 max-xl:space-y-6'>
+            <div className="">
+                <FilterPackage />
+            </div>
+            <div className="grid grid-cols-4 ">
+                <div className="col-span-4 max-sm:col-span-6 max-lg:col-span-5 border border-red-600">
+                <CardPackageList/>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
   );
 };
