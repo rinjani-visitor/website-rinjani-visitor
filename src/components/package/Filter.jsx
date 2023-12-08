dd9d9875c0c86d9011eefdbd36485cfe6501267d
Shafa Asyari Ramadhan <shafa.asyari.02@gmail.com>
2023-12-08 14:19:16 +0800

    add filter rating n price

--- src/components/package/Filter.jsx
+++ src/components/package/Filter.jsx
@@ -14,24 +14,45 @@ const price = [
   { value: 'desc', label: 'Highest' },
 ];
 
+const rating = [
+  { value: 1, label: '1 Star' },
+  { value: 2, label: '2 Star' },
+  { value: 3, label: '3 Star' },
+  { value: 4, label: '4 Star' },
+  { value: 5, label: '5 Star' },
+];
+
 export default function Filter() {
-  const [selectedOption, setSelectedOption] = useState(null);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [selectedPrice, setSelectedPrice] = useState(null);
+  const [selectedRating, setSelectedRating] = useState(null);
+  const [selectedStatus, setSelectedStatus] = useState(null);
 
   return (
     <div className="">
       <Select
-        defaultValue={selectedOption}
-        onChange={setSelectedOption}
+        defaultValue={selectedCategory}
+        onChange={setSelectedCategory}
         options={category}
-        placeholder={`Select Category`}
-        isSearchable={true}
+        placeholder={`Select Category Filter`}
+        isSearchable={false}
       />
-      {/* <Select
-        defaultValue={selectedOption}
-        onChange={setSelectedOption}
+
+      <Select
+        defaultValue={selectedPrice}
+        onChange={setSelectedPrice}
         options={price}
-        placeholder={`Select Price`}
-      /> */}
+        placeholder={`Select Price Filter`}
+        isSearchable={false}
+      />
+
+      <Select
+        defaultValue={selectedRating}
+        onChange={setSelectedRating}
+        options={rating}
+        placeholder={`Select Rating Filter`}
+        isSearchable={false}
+      />
     </div>
   );
 }
