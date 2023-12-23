6b6b60c146c05b222ea663fd13c69d2b745026aa
Shafa Asyari Ramadhan <shafa.asyari.02@gmail.com>
2023-12-23 12:40:21 +0800

    edit width

--- src/components/package/available/Unavailable.jsx
+++ src/components/package/available/Unavailable.jsx
@@ -1,6 +1,6 @@
 const Unavailable = () => {
   return (
-    <div className="w-[60px] px-2 py-0.5 rounded-md text-[10px] bg-red-700 text-white font-normal">
+    <div className="w-fit px-2 py-0.5 rounded-md text-[10px] bg-red-700 text-white font-normal">
       <p>Unavailable</p>
     </div>
   )
