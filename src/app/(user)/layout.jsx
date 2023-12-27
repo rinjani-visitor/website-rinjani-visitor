68f75b530d058ce96f4477ea1de6558901103475
Magistra <magistraagis@gmail.com>
2023-12-27 11:24:01 +0800

    remove back to top

--- src/app/(user)/layout.jsx
+++ src/app/(user)/layout.jsx
@@ -6,7 +6,7 @@ import Navbar from "@/components/navbar/Navbar";
 export default function RootLayout({ children }) {
   return (
     <section className="flex flex-col min-h-screen">
-      <ButtonScrollTop />
+      {/* <ButtonScrollTop /> */}
       <Navbar />
       <main>
         {children}
